28266a2d61b36ef9f72508305a6c179b7d670204
Pablo Rodriguez Montalvo <pablo.rodriguez@e-intelligent.es>
2020-09-10 11:50:15 +0200

    PORT is not hardcode now

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -6,12 +6,22 @@ const fetch = require("node-fetch");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/", express.static(path.join(__dirname, "../public")));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 // your API calls
 
